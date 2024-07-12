d41ceab0ee3445884892507255739373e8f3751f
Sharad <sharadd19@gmail.com>
2024-07-12 22:51:20 +0100

    Created google sheets cms for profile

--- script.js
+++ script.js
@@ -1,6 +1,28 @@
-document.title = "Sharad Patel"
+document.title = "Sharad Patel";
 // Click on download CV button to download my CV
-const cvButton = document.querySelector('.my-cv');
-cvButton.addEventListener('click', () => {
-    window.open('Sharad Patel CV.pdf', "_blank");
+const cvButton = document.querySelector(".my-cv");
+cvButton.addEventListener("click", () => {
+  window.open("Sharad Patel CV.pdf", "_blank");
 });
+
+async function getProfile() {
+  try {
+    const url =
+      "https://script.google.com/macros/s/AKfycbxGXPKima8v6GeroNLD1Hp3z811UIn1tWG7nV6UB3kJd7k5wPQBIwe8A-hq0YyYDS4s/exec";
+    const res = await fetch(url);
+
+    const data = await res.json();
+    return data;
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+async function createProfile() {
+  const profile = await getProfile();
+
+  const profileParagraph = document.getElementById("profile");
+  profileParagraph.textContent = profile.data;
+}
+
+createProfile();
